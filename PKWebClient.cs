f1ecf3f7efb6c90607a0c4420b2b11141c180885
Saad Mahmood <saad-mehmood@outlook.com>
2016-05-23 21:43:00 +0500

    Source

---
+++ PKWebClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Lazy
+{
+	public class PKWebClient:WebClient
+	{
+		protected override WebRequest GetWebRequest (Uri address)
+		{
+			var request = base.GetWebRequest (address);
+			if (request is HttpWebRequest)
+				((HttpWebRequest) request).AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+			return request;
+		}
+	}
+}
+
---
+++ TableSource.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lazy
+{
+	public class TableSource
+	{
+		public TableSource ()
+		{
+
+
+			DownloadTask = Task.Factory.StartNew (() => { });
+		}
+	
+		Task DownloadTask { get; set; }
+
+
+
+
+		void BeginDownloadingImage (string ImageUrl, NSIndexPath indexPath, UITableView tableView)
+		{
+			byte[] data = null;
+
+			UIImage returnImage = null;
+			DownloadTask = DownloadTask.ContinueWith (prevTask => {
+				try {
+					UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
+					using (var c = new GzipWebClient ())
+						data = c.DownloadData (ImageUrl);
+				} finally {
+					UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+				}
+			});
+
+			DownloadTask = DownloadTask.ContinueWith (t => {
+				returnImage = UIImage.LoadFromData (NSData.FromArray (data));
+
+
+				var cell =tableView.CellAt(indexPath);
+
+				if (cell != null)
+					cell.ImageView.Image = returnImage;
+
+				//The Download task
+			}, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.FromCurrentSynchronizationContext ());	
+		}
+	}
+}
+
